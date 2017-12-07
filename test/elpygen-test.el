3e3da0e3d410316acfd470bb8fdd8e611c54b3f2
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-12-07 13:26:01 +0000

    Test tooling + a template test

--- test/elpygen-test.el
+++ test/elpygen-test.el
@@ -3,20 +3,30 @@
 
 (require 'elpygen)
 
-(ert-deftest symbol-method-p-test ()
+(ert-deftest elpygen-symbol-method-p-test ()
   (should (elpygen-symbol-method-p "self.function_name"))
   (should (elpygen-symbol-method-p "self.function"))
   (should (not (elpygen-symbol-method-p "function_name")))
   (should (not (elpygen-symbol-method-p "function"))))
 
-(ert-deftest parse-arg-str-test ()
+(ert-deftest elpygen-parse-arg-str-test ()
   (should (equal '("1" "abc" "2")
                  (elpygen-parse-arg-str "(1, abc, 2)"))))
 
-(ert-deftest format-args-test ()
+(ert-deftest elpygen-format-args-test ()
   (should (equal ""
                  (elpygen-format-args '())))
   (should (equal "arg1, abc, arg2"
                  (elpygen-format-args '("1" "abc" "2")))))
+
+(defconst elpygen-function-template-test-function-stub "def funname(arg1, arg2):
+    pass
+")
+
+(ert-deftest elpygen-insert-template-test ()
+  (elpygen-with-temp-python-buffer ""
+    (elpygen-insert-template "funname" '("arg1" "arg2"))
+    (should (equal elpygen-function-template-test-function-stub
+                   (buffer-substring-no-properties (point-min) (point-max))))))
 
 ;;; elpygen-test.el ends here
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,17 @@
 ;;; test-helper.el --- Helpers for elpygen-test.el
 
+;; NOTE: A slightly modified copy from emacs/lisp/progmodes/python-tests.el
+(defmacro elpygen-with-temp-python-buffer (contents &rest body)
+  "Create a `python-mode' enabled temp buffer with CONTENTS.
+BODY is code to be executed within the temp buffer.  Point is
+always located at the beginning of buffer."
+  (declare (indent 1) (debug t))
+  `(with-temp-buffer
+     (let ((python-indent-guess-indent-offset nil))
+       (python-mode)
+       (yas-minor-mode)
+       (insert ,contents)
+       (goto-char (point-min))
+       ,@body)))
+
 ;;; test-helper.el ends here
