f973d2f69ad84a39d96a6e40ff67c1788c434576
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-12-08 16:24:36 +0000

    prefix private functions

--- elpygen.el
+++ elpygen.el
@@ -24,32 +24,32 @@
     (user-error "Cannot extract symbols in comments/strings "))
   (when (not (looking-at-p elpygen-funcall-re))
     (user-error "This doesn't look like a function/method call"))
-  (if-let (name (elpygen-get-def-name))
-      (if (elpygen-symbol-method-p name)
-          (elpygen-implement-method name)
-        (elpygen-implement-function name))
+  (if-let (name (elpygen--get-def-name))
+      (if (elpygen--symbol-method-p name)
+          (elpygen--implement-method name)
+        (elpygen--implement-function name))
     (user-error "Failed to find a suitable symbol")))
 
-(defun elpygen-symbol-method-p (symbol-name)
+(defun elpygen--symbol-method-p (symbol-name)
   (string-prefix-p "self." symbol-name t))
 
-(defun elpygen-implement-function (name)
-  (let ((arglist (elpygen-get-arglist)))
-    (elpygen-prepare-function-insert-point)
-    (elpygen-insert-template elpygen-function-template
-                             name
-                             arglist)))
+(defun elpygen--implement-function (name)
+  (let ((arglist (elpygen--get-arglist)))
+    (elpygen--prepare-function-insert-point)
+    (elpygen--insert-template elpygen-function-template
+                              name
+                              arglist)))
 
-(defun elpygen-implement-method (name)
-  (unless (elpygen-within-method-p)
+(defun elpygen--implement-method (name)
+  (unless (elpygen--within-method-p)
     (user-error "Can only implement a method from within a method of a class"))
-  (let ((arglist (elpygen-get-arglist)))
-    (elpygen-prepare-method-insert-point)
-    (elpygen-insert-template elpygen-method-template
-                             (seq-subseq name 5)
-                             arglist)))
+  (let ((arglist (elpygen--get-arglist)))
+    (elpygen--prepare-method-insert-point)
+    (elpygen--insert-template elpygen-method-template
+                              (seq-subseq name 5)
+                              arglist)))
 
-(defun elpygen-within-method-p ()
+(defun elpygen--within-method-p ()
   (when-let ((defun-info (python-info-current-defun))
              (defun-info-parts (split-string defun-info "\\."))
              (first-part (first defun-info-parts))
@@ -59,17 +59,17 @@
     (and (string-prefix-p "def" typed-defun-info)
          (>= (length defun-info-parts) 2))))
 
-(defun elpygen-insert-template (template name arglist)
+(defun elpygen--insert-template (template name arglist)
   (yas-expand-snippet template nil nil
                       `((fun ,name)
-                        (args ,(elpygen-format-args arglist)))))
+                        (args ,(elpygen--format-args arglist)))))
 
-(defun elpygen-get-def-name ()
+(defun elpygen--get-def-name ()
   (with-syntax-table python-dotty-syntax-table
     (when-let ((funname (thing-at-point 'symbol)))
       (substring-no-properties funname))))
 
-(defun elpygen-get-arglist ()
+(defun elpygen--get-arglist ()
   (save-excursion
     (with-syntax-table python-dotty-syntax-table
       (when (symbol-at-point)
@@ -77,13 +77,13 @@
         (skip-chars-forward "[:blank:]")
         (when (looking-at-p "(")
           (when-let (sexp (thing-at-point 'sexp))
-            (elpygen-parse-arg-str sexp)))))))
+            (elpygen--parse-arg-str sexp)))))))
 
-(defun elpygen-parse-arg-str (arg-str)
+(defun elpygen--parse-arg-str (arg-str)
   (split-string (substring-no-properties arg-str 1 -1)
                 "," t split-string-default-separators))
 
-(defun elpygen-format-args (arg-list)
+(defun elpygen--format-args (arg-list)
   (let ((counter 0))
     (cl-flet
         ((format-arg (arg)
@@ -93,7 +93,7 @@
                        (concat "arg" (number-to-string counter)))))
       (string-join (mapcar #'format-arg arg-list) ", "))))
 
-(defun elpygen-prepare-function-insert-point ()
+(defun elpygen--prepare-function-insert-point ()
   (beginning-of-line)
   (forward-line)
   (while (> (current-indentation) 0)
@@ -101,7 +101,7 @@
     (forward-line))
   (newline 2))
 
-(defun elpygen-prepare-method-insert-point ()
+(defun elpygen--prepare-method-insert-point ()
   (end-of-defun)
   (open-line 1)
   (python-indent-line-function))
--- test/elpygen-test.el
+++ test/elpygen-test.el
@@ -3,54 +3,54 @@
 
 (require 'elpygen)
 
-(ert-deftest elpygen-symbol-method-p-test ()
-  (should (elpygen-symbol-method-p "self.function_name"))
-  (should (elpygen-symbol-method-p "self.function"))
-  (should (not (elpygen-symbol-method-p "function_name")))
-  (should (not (elpygen-symbol-method-p "function"))))
+(ert-deftest elpygen--symbol-method-p-test ()
+  (should (elpygen--symbol-method-p "self.function_name"))
+  (should (elpygen--symbol-method-p "self.function"))
+  (should (not (elpygen--symbol-method-p "function_name")))
+  (should (not (elpygen--symbol-method-p "function"))))
 
-(ert-deftest elpygen-parse-arg-str-test ()
+(ert-deftest elpygen--parse-arg-str-test ()
   (should (equal '("1" "abc" "2")
-                 (elpygen-parse-arg-str "(1, abc, 2)"))))
+                 (elpygen--parse-arg-str "(1, abc, 2)"))))
 
-(ert-deftest elpygen-format-args-test ()
+(ert-deftest elpygen--format-args-test ()
   (should (equal ""
-                 (elpygen-format-args '())))
+                 (elpygen--format-args '())))
   (should (equal "arg1, abc, arg2"
-                 (elpygen-format-args '("1" "abc" "2")))))
+                 (elpygen--format-args '("1" "abc" "2")))))
 
-(ert-deftest elpygen-insert-template-test ()
+(ert-deftest elpygen--insert-template-test ()
   (elpygen-with-temp-python-buffer ""
-    (elpygen-insert-template elpygen-function-template
-                             "funname"
-                             '("arg1" "arg2"))
+    (elpygen--insert-template elpygen-function-template
+                              "funname"
+                              '("arg1" "arg2"))
     (let ((expected "def funname(arg1, arg2):
     pass
 "))
       (should (equal expected
                      (buffer-substring-no-properties (point-min) (point-max)))))))
 
-(ert-deftest elpygen-get-def-name-test ()
+(ert-deftest elpygen--get-def-name-test ()
   (elpygen-with-temp-python-buffer
-   "
+      "
 function_name()
 
 self.method_name()
 
 "
-   (elpygen-look-at "n_name")
-   (should (equal "function_name"
-                  (elpygen-get-def-name)))
+    (elpygen-look-at "n_name")
+    (should (equal "function_name"
+                   (elpygen--get-def-name)))
 
-   (elpygen-look-at "thod")
-   (should (equal "self.method_name"
-                  (elpygen-get-def-name)))
+    (elpygen-look-at "thod")
+    (should (equal "self.method_name"
+                   (elpygen--get-def-name)))
 
-   (forward-line)
-   (should (eq nil
-               (elpygen-get-def-name)))))
+    (forward-line)
+    (should (eq nil
+                (elpygen--get-def-name)))))
 
-(ert-deftest elpygen-get-arglist-test ()
+(ert-deftest elpygen--get-arglist-test ()
   (elpygen-with-temp-python-buffer "
 with_empty_args()
 
@@ -65,21 +65,21 @@ self.method_name(marg1)
 "
     (elpygen-look-at "empty_args")
     (should (equal '()
-                   (elpygen-get-arglist)))
+                   (elpygen--get-arglist)))
 
     (elpygen-look-at "no_args")
     (should (equal '()
-                   (elpygen-get-arglist)))
+                   (elpygen--get-arglist)))
 
     (elpygen-look-at "of_args")
     (should (equal '("arg1" "arg2")
-                   (elpygen-get-arglist)))
+                   (elpygen--get-arglist)))
 
     (elpygen-look-at "elf.")
     (should (equal '("marg1")
-                   (elpygen-get-arglist)))))
+                   (elpygen--get-arglist)))))
 
-(ert-deftest elpygen-within-method-p-test ()
+(ert-deftest elpygen--within-method-p-test ()
   (elpygen-with-temp-python-buffer "
 
 a_symbol
@@ -100,36 +100,36 @@ class Class:
 after_class
 "
     (elpygen-look-at "_sym")
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (forward-line)
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (elpygen-look-at "in_definition")
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (elpygen-look-at "_funcall")
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (elpygen-look-at "atic_property")
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (elpygen-look-at "w_meth")
-    (should (elpygen-within-method-p))
+    (should (elpygen--within-method-p))
 
     (forward-line)
-    (should (not (elpygen-within-method-p)))
+    (should (not (elpygen--within-method-p)))
 
     (elpygen-look-at "ter_class")
-    (should (not (elpygen-within-method-p)))))
+    (should (not (elpygen--within-method-p)))))
 
-(ert-deftest elpygen-prepare-function-insert-point-simple-test ()
+(ert-deftest elpygen--prepare-function-insert-point-simple-test ()
   (elpygen-with-temp-python-buffer "
 a_funcall()
 other_call()
 "
     (elpygen-look-at "_fun")
-    (elpygen-prepare-function-insert-point)
+    (elpygen--prepare-function-insert-point)
     (should (= 5 (line-number-at-pos)))
     (should (equal "
 a_funcall()
@@ -141,14 +141,14 @@ other_call()
                                                    (point-max))))))
 
 
-(ert-deftest elpygen-prepare-function-insert-point-block-test ()
+(ert-deftest elpygen--prepare-function-insert-point-block-test ()
   (elpygen-with-temp-python-buffer "
 if True:
     a_funcall()
 other_call()
 "
     (elpygen-look-at "_fun")
-    (elpygen-prepare-function-insert-point)
+    (elpygen--prepare-function-insert-point)
     (should (= 6 (line-number-at-pos)))
     (should (equal "
 if True:
@@ -160,7 +160,7 @@ other_call()
                    (buffer-substring-no-properties (point-min)
                                                    (point-max))))))
 
-(ert-deftest elpygen-prepare-function-insert-point-function-test ()
+(ert-deftest elpygen--prepare-function-insert-point-function-test ()
   (elpygen-with-temp-python-buffer "
 def function(arg, arg):
     a_funcall()
@@ -168,7 +168,7 @@ def function(arg, arg):
 other_call()
 "
     (elpygen-look-at "_fun")
-    (elpygen-prepare-function-insert-point)
+    (elpygen--prepare-function-insert-point)
     (should (= 7 (line-number-at-pos)))
     (should (equal "
 def function(arg, arg):
@@ -181,7 +181,7 @@ other_call()
                    (buffer-substring-no-properties (point-min)
                                                    (point-max))))))
 
-(ert-deftest elpygen-prepare-function-insert-point-method-test ()
+(ert-deftest elpygen--prepare-function-insert-point-method-test ()
   (elpygen-with-temp-python-buffer "
 class Bla():
     def function(arg, arg):
@@ -190,7 +190,7 @@ class Bla():
 other_call()
 "
     (elpygen-look-at "_funcall")
-    (elpygen-prepare-function-insert-point)
+    (elpygen--prepare-function-insert-point)
     (should (= 8 (line-number-at-pos)))
     (should (equal "
 class Bla():
